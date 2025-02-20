fe1fa83b32fb187bb775c9a99508827ec7961cdd
tvkeprof <wefousoul@gmail.com>
2025-02-21 00:11:59 +0800

    s

--- src/components/SignUpPage.tsx
+++ src/components/SignUpPage.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+
 import { ChevronLeft } from "lucide-react";
 import { useEffect } from "react";
-import { object, string, number, date, InferType } from "yup";
 export const SignUpPage = () => {
   const fe = async () => {
     try {
@@ -18,15 +18,19 @@ export const SignUpPage = () => {
   });
   return (
     <div className="w-screen h-screen flex justify-between items-center">
-      <div className="bg-white w-[25%] h-[30%] flex flex-col space-y-4 p-4 justify-center m-auto">
-        <ChevronLeft />
-        <h1>Create your account</h1>
-        <p>Sign up to explore your favorite dishes.</p>
+      <div className="bg-white w-[35%] h-[30%] flex flex-col space-y-4 p-4 justify-center m-auto">
+        <ChevronLeft className="w-[200px] h-[200px]"/>
+        <h1 className="font-bold text-2xl">Create your account</h1>
+        <p className="text-[#71717A] text-lg">Sign up to explore your favorite dishes.</p>
 
-        <input placeholder="Enter your email address" />
-        <button className="bg-gray-300" onClick={(userSchema) => {}}>
+        <input className="border p-2 rounded-lg"  placeholder="Enter your email address" />
+        <button className="bg-gray-300 p-3 rounded-lg" onClick={(userSchema) => {}}>
           Let's go
         </button>
+        <div className="flex gap-4 justify-center">
+          <p className="text-[#71717A] text-lg">Already have an account?</p>
+          <a className="text-blue-500 text-lg">Log in</a>
+        </div>
       </div>
       <div className="  w-[55%] h-[80%] mr-[100px]">
         <img
