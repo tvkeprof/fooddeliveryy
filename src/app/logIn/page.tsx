c2bc1efa12308209d223a0026e9664206af7e1a0
tvkeprof <wefousoul@gmail.com>
2025-03-18 19:26:39 +0800

    f

--- src/app/logIn/page.tsx
+++ src/app/logIn/page.tsx
@@ -1,51 +1,84 @@
-
-
 "use client";
 
 import { ChevronLeft } from "lucide-react";
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-const LogIn = ({})=> {
+const LogIn = ({}) => {
+  const Router = useRouter();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const Router = useRouter();
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:9999/login",
+        formData
+      );
+      const { token } = response.data;
+      localStorage.setItem("token", token);
+      console.log("Login success", response.data);
+      if (response.data.role === "ADMIN") {
+        await Router.push("/admin");
+      } else {
+        await Router.push("/home");
+      }
+      //   console.log(response.data.user.role);
+    } catch (err) {
+      console.log("login failed", err);
+      alert("aldaaaaaaaaa");
+    }
+  };
 
-    return(
-        <div className="w-screen h-screen flex items-center">
-            <div className="flex flex-col bg-white w-[35%] h-[30%] space-y-4 p-4 justify-center m-auto">
+  return (
+    <div className="w-screen h-screen flex items-center">
+      <div className="flex flex-col bg-white w-[35%] h-[30%] space-y-4 p-4 justify-center m-auto">
+        <ChevronLeft />
+        <h1 className="font-bold text-2xl">Log in</h1>
+        <p>Log in to enjoy your favorite dishes</p>
 
+        <input
+          type="email"
+          name="email"
+          className="border p-2 rounded-lg"
+          placeholder="Enter your email address"
+          value={formData.email}
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        />
+        <input
+          type="password"
+          name="password"
+          className="border p-2 rounded-lg"
+          placeholder="Enter your password"
+          value={formData.password}
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        />
 
-            <ChevronLeft/>
-            <h1 className="font-bold text-2xl">Log in</h1>
-            <p>Log in to enjoy your favorite dishes</p>
+        <a className=" border">Forgor password</a>
 
-            <input type="email"
-            name="email"
-            className="border p-2 rounded-lg"
-            placeholder="Enter your email address"
-            />
-            <input
-            type="password"
-            name="password"
-            className="border p-2 rounded-lg"
-            placeholder="Enter your password"/>
-
-            <a className=" border">Forgor password</a>
-
-            <button type="submit" className="bg-gray-300 p-3 rounded-lg">Let's go</button>
-            <div className="flex gap-4 justify-center">
-                <p className="text-[#71717A] text-lg">Don't have an account?</p>
-                <a className="text-blue-500 text-lg">Sign up</a>
-            </div>
-            </div>
-            <div className="w-[55%] h-[80%] mr-[100px]">
-                <img
-                src="deliveryWithBike.png"
-                className="w-full h-full rounded-[10px]"/>
-            </div>
-
-
+        <button
+          type="submit"
+          className="bg-gray-300 p-3 rounded-lg"
+          onClick={handleLogin}
+        >
+          Let's go
+        </button>
+        <div className="flex gap-4 justify-center">
+          <p className="text-[#71717A] text-lg">Don't have an account?</p>
+          <a className="text-blue-500 text-lg">Sign up</a>
         </div>
-    )
-}
+      </div>
+      <div className="w-[55%] h-[80%] mr-[100px]">
+        <img
+          src="deliveryWithBike.png"
+          className="w-full h-full rounded-[10px]"
+        />
+      </div>
+    </div>
+  );
+};
 export default LogIn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
-"use client"
+"use client";
 import SignUpPage from "@/components/SignUpPage";
-import HomePage from "@/components/HomePage";
+// import HomePage from "@/components/HomePage";
 import { useState } from "react";
-
 
 export default function Home() {
   const [step, setStep] = useState(1);
   const nextStep = () => {
-    setStep((prevStep)=> prevStep + 1);
+    setStep((prevStep) => prevStep + 1);
   };
   const backStep = () => {
-    setStep((prevStep)=> prevStep - 1);
-  }
+    setStep((prevStep) => prevStep - 1);
+  };
   return (
     <div className="bg-white w-full h-screen">
-      {step === 1 && <SignUpPage onNext={nextStep}/>}
-      {step === 2 && <HomePage/>}
-
+      {step === 1 && <SignUpPage onNext={nextStep} />}
+      {/* {step === 2 && <HomePage/>} */}
     </div>
   );
 }
-
-
